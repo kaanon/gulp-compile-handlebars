16795861c4e1ebd29fa9065cba1d96437a66c97e
Eric Eastwood <madlittlemods@gmail.com>
2014-10-07 10:33:48 -0500

    Add `at` format to import helpers from template
    Format: `{{@helpers/myhelper}}` and `{{#@helpers/myblockhelper}}` --
    Added path map option to map a module name to complete path making it
    easier to reference helpers. Add option: `helpersAtPathMap: {}`

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var gutil = require('gulp-util');
 var through = require('through2');
 var Handlebars = require('handlebars');
 var fs = require('fs');
+var path = require("path");
 
 module.exports = function (data, opts) {
 
@@ -54,7 +55,7 @@ module.exports = function (data, opts) {
 				}
 			}
 		});
-	}
+	};
 
 
 	// Go through a partials directory array
@@ -63,7 +64,7 @@ module.exports = function (data, opts) {
 		if(typeof options.batch === 'string') options.batch = [options.batch];
 
 		options.batch.forEach(function (piece) {
-			searchDirForPartials(piece, piece.split('/').pop(), 0)
+			searchDirForPartials(piece, piece.split('/').pop(), 0);
 		});
 	}
 
@@ -79,10 +80,97 @@ module.exports = function (data, opts) {
 				partial = match[1];
 				//Only register an empty partial if the partial has not already been registered
 				if(!Handlebars.partials.hasOwnProperty(partial)){
-					Handlebars.registerPartial(partial, gutil.noop);
+					Handlebars.registerPartial(partial, '');
 				}
 			}
 		}
+	};
+
+	// options: 
+	// ```
+	//	helpersAtPathMap: {
+	//		'helpers': './src/js/hbs-helpers/'
+	//	}
+	// {{@helpers/slugify.helper
+	// {{#@helpers/include.helper}}...{{/helpers/include.helper}}
+	var registerAtPathHelpers = function(content, basePathMap) 
+	{
+		var includeHelpersRegex = /{{@([a-zA-Z-0-9\.\/\~]+)/g;
+		var includeBlockHelpersRegex = /{{#@([a-zA-Z-0-9\.\/\~]+)/g;
+		var closingIncludeBlockHelpers = /{{\/([a-zA-Z-0-9\.\/\~]+)/g;
+
+
+		// replace internal requires with helper form
+		var sanitize = function(name) {
+			return name.replace('/', '-').replace('.', '').replace('~', '_');
+		};
+
+		// Grab all of the helpers out of the template
+		var helpers = [];
+		// Work out the normal helpers
+		content = content.replace(includeHelpersRegex, function(match, dep) {
+			helpers.push(dep);
+			return '{{' + sanitize(dep);
+		});
+		// Work out the block helpers
+		content = content.replace(includeBlockHelpersRegex, function(match, dep) {
+			var helperId = dep;
+			helpers.push(helperId);
+			return '{{#' + sanitize(helperId);
+		});
+		// Work out closing to the block helper {{/helper}}
+		content = content.replace(closingIncludeBlockHelpers, function(match, dep) {
+			return '{{/' + sanitize(dep);
+		});
+
+		var arrayUnique = function(a) {
+			return a.reduce(function(p, c) {
+				if (p.indexOf(c) < 0) p.push(c);
+				return p;
+			}, []);
+		};
+
+		// No repeats (because it is unnecessary)
+		helpers = arrayUnique(helpers);
+
+		// We can only
+		if(Object.keys(basePathMap).length > 0)
+		{
+			// Register the helpers
+			for (var i = 0; i < helpers.length; i++)
+			{
+				var helperPath = helpers[i];
+
+				// Replace any defined module names with the appropriate relative paths
+				Object.keys(basePathMap).forEach(function(mapKey, index, array) {
+					var basePath = basePathMap[mapKey];
+					helperPath = helperPath.replace(new RegExp("^(" + mapKey + ")/"), basePath);
+				});
+
+				// Turn the path from relative from the calling file to absolute
+				helperPath = path.resolve(process.cwd(), helperPath);
+				if (process.platform === "win32") {
+					// Switch backslash to forward slash
+					helperPath = helperPath.replace(/\\/g, "/");
+				}
+
+
+				var helperFunc = null;
+				try {
+					// Try to get the helper function
+					helperFunc = require(helperPath);
+				} catch(e) {
+
+				}
+				if(helperFunc !== null)
+				{
+					// Actually register the helper, if we could find it
+					Handlebars.registerHelper(sanitize(helpers[i]), helperFunc);
+				}
+			}
+		}
+
+		return content;
 	};
 
 
@@ -102,6 +190,8 @@ module.exports = function (data, opts) {
 			if(options.ignorePartials){
 				mockPartials(fileContents);
 			}
+			fileContents = registerAtPathHelpers(fileContents, options.helpersAtPathMap || {});
+
 			var template = Handlebars.compile(fileContents);
 			file.contents = new Buffer(template(data));
 		} catch (err) {
